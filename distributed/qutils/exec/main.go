c8baaedcfd5abbb36b24bbcfc5a7c7765ccf61bf
Ralf Ehret <ralf@ehret-family.com>
2019-01-11 21:19:38 +0100

    test for encoding/decoding

--- distributed/qutils/exec/main.go
+++ distributed/qutils/exec/main.go
@@ -3,25 +3,51 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
-	"log"
+	"fmt"
+	"time"
 
 	"github.com/Snoopy1964/powerPlantMonitor/distributed/dto"
 )
 
 func main() {
-	msgBody := `N/+BAwEBDVNlbnNvck1lc3NhZ2UB/4IAAQMBBE5hbWUBDAABBVZhbHVlAQgAAQNUc3QB/4QAAAAQ/4MFAQEEVGltZQH/hAAAADP/ggETYm9pbGVyX3ByZXNz
-dXJlX291dAH4y/ZJPQi8CUABDwEAAAAO08qKAzVJj9gAPAA=`
-	b := []byte(msgBody)
+	/*
+			msgBody := `N/+BAwEBDVNlbnNvck1lc3NhZ2UB/4IAAQMBBE5hbWUBDAABBVZhbHVlAQgAAQNUc3QB/4QAAAAQ/4MFAQEEVGltZQH/hAAAADP/ggETYm9pbGVyX3ByZXNz
+		dXJlX291dAH4y/ZJPQi8CUABDwEAAAAO08qKAzVJj9gAPAA=`
+			b := []byte(msgBody)
 
-	log.Printf("String: %s", msgBody)
-	log.Printf("Bytes : %v", b)
+			log.Printf("String: %s", msgBody)
+			log.Printf("Bytes : %v", b)
 
-	buf := bytes.NewReader(b)
-	dec := gob.NewDecoder(buf)
+			buf := bytes.NewReader(b)
+			dec := gob.NewDecoder(buf)
 
-	sd := &dto.SensorMessage{}
-	dec.Decode(sd)
+			sd := &dto.SensorMessage{}
+			dec.Decode(sd)
 
-	log.Printf("\n\nDecoded String: %v", sd)
+			log.Printf("\n\nDecoded String: %v", sd)
+
+	*/
+	fmt.Println("... Started ....")
+	buf := new(bytes.Buffer)
+
+	reading := dto.SensorMessage{
+		Name:  "hallo",
+		Value: 3.1415,
+		Tst:   time.Now(),
+	}
+
+	buf.Reset()
+	enc := gob.NewEncoder(buf)
+	enc.Encode(reading)
+
+	fmt.Printf("Bytes:  %v\n", buf.Bytes())
+	fmt.Printf("String: %s\n", string(buf.Bytes()))
+
+	// ------------------- decoding
+	r := bytes.NewReader(buf.Bytes())
+	d := gob.NewDecoder(r)
+	sd := new(dto.SensorMessage)
+	d.Decode(sd)
+	fmt.Printf("Decoded: %v\n", sd)
 
 }
