e1120c13f5e7ebf93af13efec5d9191434c0f46c
Ralf Ehret <ralf.ehret@sap.com>
2019-01-23 17:40:58 +0100

    Resilience during startup version 0.1

--- distributed/coordinator/exec/main.go
+++ distributed/coordinator/exec/main.go
@@ -21,6 +21,4 @@ func main() {
 		fmt.Println(msg)
 	}
 
-	// var a string
-	// fmt.Scanln(&a)
 }
--- distributed/qutils/queueutils.go
+++ distributed/qutils/queueutils.go
@@ -6,6 +6,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/snoopy1964/powerPlantMonitor/distributed/dto"
 
@@ -18,9 +19,38 @@ const SensorDiscoveryExchange = "SensorDiscovery"
 // queue for persistent readings
 const PersistReadingsQueue = "PersistReading"
 
+func connectToRabbitMQ(url string) (conn *amqp.Connection, err error) {
+	t0 := time.Now()
+	var err0 error
+	for {
+		conn, err = amqp.Dial(url)
+		if err == nil {
+			break
+		} else {
+			if err0 == nil {
+				log.Printf("%s\n", err.Error())
+			} else {
+				if err0.Error() != err.Error() {
+					log.Printf("%s\n", err.Error())
+				}
+			}
+			err0 = err
+		}
+
+		time.Sleep(1000 * time.Millisecond)
+		log.Printf("Trying to reconnect to RabbitMQ at %s\n", url)
+		if time.Now().Sub(t0) > 30*time.Second {
+			log.Println("Timeout reconnecting....")
+			break
+		}
+		err0 = err
+	}
+	return conn, err
+}
+
 func GetChannel(url string) (*amqp.Connection, *amqp.Channel) {
 	log.Println("connection to Messagebroker with connection string: ", url)
-	conn, err := amqp.Dial(url)
+	conn, err := connectToRabbitMQ(url)
 	failOnError(err, "Failed to establish a connection to message broker")
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to get channel for connection")
